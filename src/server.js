fb033f1c6827baf93aca31ce20f908051569d67f
Daria <danylkova28@gmail.com>
2026-09-04 17:16:05 +0300

    fix: middleware order

--- src/server.js
+++ src/server.js
@@ -6,15 +6,6 @@ import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
-
 app.use(express.json());
 app.use(cors());
 app.use(
@@ -33,6 +24,15 @@ app.use(
     },
   }),
 );
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
 
 app.get('/test-error', (req, res) => {
   throw new Error('Simulated server error');
